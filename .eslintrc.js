fd2a2a399bb3d1cc170a081d0d5b29312edc28a2
JUAN GOMEZ <juandiego.gomez1@utp.edu.co>
2025-01-31 00:45:27 -0500

    configuracion linter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+      es6: true,
+      node: true,
+    },
+    extends: 'standard',
+    parserOptions: {
+      ecmaVersion: 2018,
+    },
+    rules: {
+      // Añade o modifica reglas según tus preferencias
+    },
+  };
+  
---
+++ index.js
@@ -0,0 +1,8 @@
+let nombre = "Juan Diego";
+let apellido = "Gonzalez";
+if (nombre == apellido){
+        console.log("Son iguales")
+}
+else{
+    console.log("No son iguales")
+}
